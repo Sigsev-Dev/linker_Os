c0c8e41f6c31a3fa4514f707f773c3ae01ae3b1e
Sigsev-Dev <devpant.btech.cs19@iiitranchi.ac.in>
2024-02-15 12:32:10 -0500

    restructured code

--- lab1_assign/final.cpp
+++ lab1_assign/final.cpp
@@ -1,3 +1,17 @@
+/*********************************************************************************************************
+*
+*
+*
+* Operating System Assignment
+* Lab 1
+* Submitted by: Dev Pant (dp3887)
+* New York University
+*
+*
+*
+*///******************************************************************************************************
+
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -19,53 +33,8 @@
 #include <regex>
 using namespace std;
 
-#define rep(i,b) for(ll (i) = 0 ;i<(b);i++)
-#define per(i,b) for(ll (i) = ll(b)-1;i>=0;i--)
-#define reep(i,a,b) for(ll (i)=ll(a);i<(b);i++)
-#define peer(i,b,a) for(ll (i)=ll(b)-1;i>=(a);i--)
-#define all(x) x.begin(),(x).end()
-#define pb(x) push_back(x)
-#define mp(x, y) make_pair(x, y)
-#define sz(x) (ll) x.size()
-#define len() length()
-#define ff first
-#define ss second
-#define ret return
-
-#define printvec(a) for(auto _ : a) cout << _ << ' '; cout << '\n';
-#define print2(a, b) cout << a << ' ' << b << endl;
-#define print3(a, b, c) cout << a << ' ' << b << ' ' << c << endl;
-#define print4(a, b, c, d) cout << a << ' ' << b << ' ' << c << ' ' << d << endl;
-
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
 
 typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<ll> vll;
-typedef vector<vll> vvl;
-typedef vector<char> vc;
-typedef vector<vc> vvc;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<string, string> pss;
-typedef map<int, int> mii;
-typedef unordered_map<int, int> umap_ii;
-typedef unordered_map<string, int> umap_si;
-
-/**
- * Limits in C++ for reference
- * _____________________________________________________________________________________
- * |Sr| Macro Name | Description                     | Value
- * |No|____________|_________________________________|__________________________________
- * |1.| ULLONG_MAX | Maximum value unsigned long long| 18,446,744,073,709,551,615 (10^20)
- * |2.| LLONG_MAX  | Maximum value long long         | 9,223,372,036,854,775,807 (10^19)
- * |3.| LLONG_MIN  | Minimum value long long         |-9,223,372,036,854,775,808 -1*(10^19)
- * |4.| INT_MAX    | Maximum value int               | 2,147,483,647 (10^10)
- * |5.| INT_MIN    | Minimum value int               |-2,147,483,648 (10^10)
-*/
 
 
 const int Machine_Size=512;
@@ -74,24 +43,6 @@ string line;
 const char* delimeter= "\t\n";
 bool fileNotEnd;
 
-
-// void solve()
-// {
-    
-// char s[1000];
-//         scanf("%s",s);
-//         for(int i = 0; i < strlen(s); ++i ) {
-//             bool flag = false;
-//             if(s[i] == 'U' && s[i-1] == 'W' && s[i+1] == 'B')   continue;
-//             else if(s[i] == 'W' && s[i+1] == 'U' && s[i+2] == 'B')  continue;
-//             else if(s[i] == 'B' && s[i-1] == 'U' && s[i-2] == 'W')  continue;
-//             printf("%c",s[i] );
-//             if(s[i+1] == 'W' && s[i+2] == 'U' && s[i+3] == 'B')
-//                 printf(" ");
-//         }
-//         printf("\n");   
-    
-// }
 
 void parseerror(int errcode)
 {
@@ -127,15 +78,19 @@ struct Module{
 };
 
 std::vector<Module> modules;
+size_t lastTokenEndPos=0;
 
 string fetchNextToken(std::ifstream& fileStream) {
     static string currentToken;
     static std::istringstream currentStream;
     static bool firstCall = true;
+    static string lastLine = "";
 
     if (firstCall || currentStream.eof()) {
+        lastLine=line;
         if (!getline(fileStream, line)) {
             fileNotEnd = false; // End of file
+            offset=lastTokenEndPos;
             return "";
         }
         currentStream.clear(); // Clear any errors.
@@ -143,6 +98,7 @@ string fetchNextToken(std::ifstream& fileStream) {
         firstCall = false;
         lineNum++;
         offset = 1; // Reset offset with each new line
+        lastTokenEndPos=line.size()+1;
     }
 
     if (!(currentStream >> currentToken)) {
@@ -151,7 +107,27 @@ string fetchNextToken(std::ifstream& fileStream) {
     }
 
     // Calculate offset for the next token by finding the position of the current token in the line
-    offset = line.find(currentToken, offset - 1) + 1;
+    //size_t endPos = currentStream.tellg();
+    //offset = line.find(currentToken, offset - 1) + 1;
+    // size_t startPos = currentStream.str().find_first_not_of(" \t", lastTokenEndPos);
+    
+    // if (startPos != string::npos) {
+    //     // Calculate the offset for the next token
+    //     // Offset in the file stream starts from 0, but we want to show it as starting from 1 for the user
+    //     offset = startPos+1;
+    // } else {
+    //     // When transitioning between lines, reset the offset
+    //     offset = lastTokenEndPos+1;
+    // }
+    // size_t tokenEndPos = offset + currentToken.length() - 1;
+    // lastTokenEndPos = tokenEndPos;
+    offset=currentStream.tellg();
+    if(offset<0){
+        offset=lastTokenEndPos-currentToken.size();
+    }
+    else{
+        offset=offset-currentToken.size()+1;
+    }
     return currentToken;
 }
 
@@ -169,46 +145,30 @@ bool isValidAddress(const string& input) {
     return validAddresses.find(input) != validAddresses.end();
 }
 
-// void demoSymbolTable() {
-//     // Adding some symbols for demonstration
-//     createSymbol("START", 100, 1);
-//     createSymbol("LOOP", 150, 1);
-//     createSymbol("END", 200, 1);
-
-//     // Attempting to add a symbol that already exists
-//     createSymbol("LOOP", 250, 2);
-
-//     // Printing the symbol table
-//     std::cout << "Symbol Table\n";
-//     for (const auto& entry : symbol_table) {
-//         std::cout << entry.first << "=" << entry.second.position;
-//         if (entry.second.isMultDef) {
-//             std::cout << " Error: This variable is multiple times defined; first value used";
+// void createSymbol(const std::string& symbolName, int position, int moduleNumber) {
+//     // Check if the symbol already exists in the table
+//     auto it = symbol_table.find(symbolName);
+//     if (it != symbol_table.end()) {
+//         // If the symbol exists, mark it as multiply defined
+//         if (it->second.moduleNumber == moduleNumber) { // Adjust this check based on your logic
+//             cout << "Warning: Module " << moduleNumber-1 << ": " << symbolName << " redefinition ignored" << endl;
 //         }
-//         std::cout << std::endl;
+//         //cout << "Warning: Module " << moduleNumber-1 << ": " << symbolName << " redefinition ignored" << endl;
+//         else{it->second.isMultDef = true;}
+//     } else {
+//         // If the symbol doesn't exist, add it to the table
+//         Symbol newSymbol;
+//         newSymbol.symbol = symbolName;
+//         newSymbol.position = position;
+//         newSymbol.moduleNumber = moduleNumber;
+//         newSymbol.isMultDef = false; // This is already the default value
+//         newSymbol.used = false;
+//         newSymbol.value = position; // Assuming 'value' should be set to 'position', adjust if needed
+
+//         symbol_table[symbolName] = newSymbol;
+//         // symbolRange.push_back()
 //     }
 // }
-
-void createSymbol(const std::string& symbolName, int position, int moduleNumber) {
-    // Check if the symbol already exists in the table
-    auto it = symbol_table.find(symbolName);
-    if (it != symbol_table.end()) {
-        // If the symbol exists, mark it as multiply defined
-        cout << "Warning: Module " << moduleNumber-1 << ": " << symbolName << " redefinition ignored" << endl;
-        it->second.isMultDef = true;
-    } else {
-        // If the symbol doesn't exist, add it to the table
-        Symbol newSymbol;
-        newSymbol.symbol = symbolName;
-        newSymbol.position = position;
-        newSymbol.moduleNumber = moduleNumber;
-        newSymbol.isMultDef = false; // This is already the default value
-        newSymbol.used = false;
-        newSymbol.value = position; // Assuming 'value' should be set to 'position', adjust if needed
-
-        symbol_table[symbolName] = newSymbol;
-    }
-}
 
 int fetchInteger(std::ifstream& fileStream) {
     string token = fetchNextToken(fileStream);
@@ -241,14 +201,20 @@ string fetchAddress(std::ifstream& fileStream) {
     return token;
 }
 
-void printSymbolTable() {
+void printSymbolTable(vector<Symbol> symbols, vector<int> module_addresses) {
     cout << "Symbol Table" << std::endl;
-    for (const auto& entry : symbol_table) {
-        const std::string& symbol = entry.first;
-        const Symbol& symData = entry.second;
-
-        cout << symbol << "=" << symData.position;
-        if (symData.isMultDef) {
+    for (const auto& symbol : symbols) {
+        // const std::string& symbol = entry.first;
+        //const Symbol& symData = symbol_table[symbol.symbol];
+        //cout<<"tester "<<symbol.position<<" "<<symbol.moduleNumber<<" "<<module_addresses[symbol.moduleNumber]<<endl;
+        //cout<<"position: ="<<symbol_table[symbol.symbol].position<<" "<<symbol.moduleNumber<<endl;
+        if(symbol.moduleNumber==0){
+            symbol_table[symbol.symbol].position=symbol_table[symbol.symbol].position+0;
+        }else{
+        symbol_table[symbol.symbol].position=symbol_table[symbol.symbol].position+module_addresses[symbol.moduleNumber-1];
+        }
+        cout << symbol.symbol << "=" << symbol_table[symbol.symbol].position;
+        if (symbol_table[symbol.symbol].isMultDef) {
             std::cout << " Error: This variable is multiple times defined; first value used";
         }
         cout << std::endl;
@@ -257,6 +223,8 @@ void printSymbolTable() {
 
 
 void firstPass(const std::string& input_file) {
+    vector<Symbol> symbols;
+
     std::ifstream fileStream(input_file);
     if (!fileStream) {
         std::cerr << "Cannot open file" << std::endl;
@@ -264,11 +232,13 @@ void firstPass(const std::string& input_file) {
     }
 
     int total_code = 0;
-    int module_num = 1;
-
+    int module_num = 0;
+    //int global_module_address=0;
+    vector<int> module_addresses;
     while (fileStream.peek() != EOF) {
         Module module;
-        module.base_address=total_code;
+        
+        //module.base_address=total_code;
         
         
         // Definition list
@@ -277,12 +247,37 @@ void firstPass(const std::string& input_file) {
             parseerror(4);
             return;
         }
-        
+        vector<Symbol> tempSymbols;
+
+
         for(int i = 0; i < defcount; i++) {
             std::string symbol = fetchSymbol(fileStream);
             int relativePosition = fetchInteger(fileStream);
-            int absolutePosition = module.base_address + relativePosition;
-            createSymbol(symbol, absolutePosition, module_num);
+            // if (relativePosition >= module.codecount) {
+            //     // cout<<"flag check!!!!!"<<module.codecount<<endl;
+            //     cout << "Warning: Module " << module_num-1 << ": " << symbol << "=" << relativePosition << " valid=[0.." << module.codecount-1 << "] assume zero relative" << endl;
+            //     relativePosition = 0; // Adjust to zero relative
+            // }
+            
+            Symbol newSymbol;
+            newSymbol.symbol = symbol;
+            newSymbol.position = relativePosition;
+            newSymbol.moduleNumber = module_num;
+            newSymbol.isMultDef = false; // This is already the default value
+            newSymbol.used = false;
+            newSymbol.value = relativePosition; // Assuming 'value' should be set to 'position', adjust if needed
+            
+            if(symbol_table.find(symbol) == symbol_table.end()){
+                symbol_table[symbol] = newSymbol;
+                symbols.push_back(newSymbol);
+            }
+            else{
+                newSymbol.isMultDef=true;
+                symbol_table[symbol].isMultDef=true;
+            }
+            tempSymbols.push_back(newSymbol);
+            //int absolutePosition = module.base_address + relativePosition;
+            //createSymbol(symbol, absolutePosition, module_num);
         }
         
         // Use list
@@ -299,12 +294,36 @@ void firstPass(const std::string& input_file) {
 
         // Program text
         int codecount = fetchInteger(fileStream);
-        module.base_address = total_code; // Assuming Module has a base_address field
+        //checking for exceeding module size
+        for (auto& sym : tempSymbols) {
+            auto it = symbol_table.find(sym.symbol);
+            //symbol_table[sym.symbol].position = sym.position + module.base_address;
+                
+                if(sym.isMultDef==true){
+                    cout << "Warning: Module " << sym.moduleNumber << ": " << sym.symbol << " redefinition ignored" << endl;
+                }
+            
+            else if (sym.position >= codecount) {
+                cout << "Warning: Module " << module_num << ": " << sym.symbol << "=" << sym.position << " valid=[0.."<<codecount-1<<"] assume zero relative" << endl;
+                //sym.position = 0; // Adjust to zero relative
+                symbol_table[sym.symbol].position=0;
+            }
+            //sym.position=sym.position + module.base_address;
+            
+            //symbol_table[sym.symbol].position = sym.position + module.base_address;
+            
+            //createSymbol(sym.first, sym.second + module.base_address , module_num);
+        }
+
+        module.base_address=total_code;
         total_code += codecount;
+        //cout<<"total_code="<<total_code<<endl;
+        //module.base_address = total_code; // Assuming Module has a base_address field
         if(codecount < 0 || total_code > 512){
             parseerror(6);
             return;
         }
+        module_addresses.push_back(total_code);
         module.codecount = codecount;
         
         for (int i = 0; i < codecount; i++) {
@@ -316,7 +335,7 @@ void firstPass(const std::string& input_file) {
         
         module_num++;
     }
-
+    printSymbolTable(symbols,module_addresses);
 }
 
 
@@ -329,8 +348,8 @@ void secondPass(const std::string& input_file) {
 
     int count = 0; // Memory map count
     int module_num = 0; // Current module index
-
-
+    unordered_map<string,bool> checker_for_symbols_in_DefList;
+    map<string,bool> symbol_usage_status;
     std::cout << "\nMemory Map\n";
 
     while (module_num < modules.size()) {
@@ -344,6 +363,7 @@ void secondPass(const std::string& input_file) {
             int relativePosition = fetchInteger(fileStream);
             // int absolutePosition = module.base_address + relativePosition;
             // createSymbol(symbol, absolutePosition, module_num);
+            checker_for_symbols_in_DefList[symbol];
         } // Assume this function skips over the def list correctly
 
         int usecount = fetchInteger(fileStream);
@@ -374,15 +394,15 @@ void secondPass(const std::string& input_file) {
                 if (operand >= modules.size()) {
                     // Handle error: Illegal module operand
                     cout << setw(3) << setfill('0') << count << ": ";
-                    cout << opcode * 1000 << setw(3) << setfill('0') << 0;
-                    cout << " Error: Illegal module operand; treated as module=0\n";
+                    cout << opcode << setw(3) << setfill('0') << 0;
+                    cout << " Error: Illegal module operand ; treated as module=0\n";
                 } else {
                     // Fetch the base address of the specified module
                     int moduleBaseAddress = modules[operand].base_address;
                     // Adjust the instruction with the module's base address
                     instr = (opcode * 1000) + moduleBaseAddress;
                     cout << setw(3) << setfill('0') << count << ": ";
-                    cout << setw(4) << setfill('0') << instr << "\n";
+                    cout << setw(4) << setfill('0') << instr << endl;
                 }
             }else if (addressmode == "I") {
                 // Immediate address mode, print as is
@@ -396,11 +416,11 @@ void secondPass(const std::string& input_file) {
                 {
                      err= "";
                 }
-                std::cout << std::setfill('0') << std::setw(3) << count << ": " << err <<  std::setw(4) << instr << "\n";
+                std::cout << std::setfill('0') << std::setw(3) << count << ": " << instr / 1000 << std::setw(3) << operand <<" "<< err << endl;
             } else if (addressmode == "A") {
                 // Absolute address mode
                 if (operand < Machine_Size) {
-                    std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << instr << "\n";
+                    std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << instr << endl;
                 } else {
                     // Handle error: Absolute address exceeds machine size
                     std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << (opcode * 1000) << " Error: Absolute address exceeds machine size; zero used\n";
@@ -410,7 +430,7 @@ void secondPass(const std::string& input_file) {
                 operand += modules[module_num].base_address; // Adjust with the module's base address
                 if (operand < modules[module_num].base_address + modules[module_num].codecount) {
                     instr = opcode * 1000 + operand;
-                    std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << instr << "\n";
+                    std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << instr << endl;
                 } else {
                     // Handle error: Relative address exceeds module size
                     std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << (opcode * 1000 + modules[module_num].base_address) << " Error: Relative address exceeds module size; relative zero used\n";
@@ -419,13 +439,14 @@ void secondPass(const std::string& input_file) {
                 // External address mode
                 if (operand < usecount) {
                     std::string symbol = uselist[operand];
+                    symbol_usage_status[symbol]=true;
                     actually_used.insert(symbol);
                     if (symbol_table.find(symbol) != symbol_table.end()) {
                         // Symbol is defined
                         Symbol& sym = symbol_table[symbol];
                         sym.used = true;
                         instr = opcode * 1000 + sym.position;
-                        std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << instr << "\n";
+                        std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << instr << endl;
                     } else {
                         // Handle error: Symbol not defined
                         std::cout << std::setfill('0') << std::setw(3) << count << ": " << std::setw(4) << (opcode * 1000) << " Error: " << symbol << " is not defined; zero used\n";
@@ -455,8 +476,8 @@ void secondPass(const std::string& input_file) {
 
     // Check for symbols that were defined but never used
     for (const auto& entry : symbol_table) {
-        if (!entry.second.used) {
-            std::cout << "\nWarning: Module " << entry.second.moduleNumber - 1 << ": " << entry.first << " was defined but never used";
+        if (!entry.second.used && !checker_for_symbols_in_DefList[entry.first]) {
+            std::cout << "\nWarning: Module " << entry.second.moduleNumber << ": " << entry.first << " was defined but never used";
         }
     }
 
@@ -494,7 +515,7 @@ int main(int argc, char* argv[]) {
     // #endif
 
     firstPass(inputFile);
-    printSymbolTable();
+    //printSymbolTable();
     secondPass(inputFile);
 
     return 0;
